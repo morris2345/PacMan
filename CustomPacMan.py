6409c734c9ebe935091acd809aab9f1457f0895d
morris2345 <117653800+morris2345@users.noreply.github.com>
2025-02-19 14:06:37 +0100

    Added fillMaze function
    fillMaze adds pills, ghosts & food to empty cells

--- CustomPacMan.py
+++ CustomPacMan.py
@@ -1,12 +1,18 @@
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
+import random
 
 class CustomPacManEnv(gym.Env):
     metadata = {"render_modes": ["human"], "render_fps": 10}
 
     def __init__(self, maze_size="normal"):
         super().__init__()
+
+        self.pill_amount = 4 #amount of pills in the maze
+        self.ghost_amount = 4 #amount of ghosts in the maze
+        self.ghosts = [] #ghost positions
+        self.pills = [] #pill positions
 
         # Define available maze files
         self.maze_files = {
@@ -17,10 +23,13 @@ class CustomPacManEnv(gym.Env):
 
         self.maze_size = maze_size
         self.grid = self.loadMaze()  # Load the maze from file
+        self.fillMaze()#Fill maze with Pills, Ghosts & Food
 
         # Define action space (UP, DOWN, LEFT, RIGHT)
         self.action_space = spaces.Discrete(4)
 
+
+    #Load the maze file containing walls & empty spaces
     def loadMaze(self):
         fileName = self.maze_files.get(self.maze_size, "mazes/normal.txt") #load maze file, default normal
 
@@ -29,3 +38,30 @@ class CustomPacManEnv(gym.Env):
 
         maze = np.array([[int(char) for char in line.strip()] for line in lines], dtype=np.int8)
         return maze
+    
+    #Fill the maze with Pills, Ghosts & Food
+    def fillMaze(self):
+        empty_cells = list(zip(*np.where(self.grid == 0)))
+
+        #Place Pills
+        for i in range(self.pill_amount):
+            if(empty_cells):
+                x, y = random.choice(empty_cells)
+                self.grid[x, y] = 2 #Add Pill (2) to grid
+                empty_cells.remove((x, y)) #Remove empty cell from list
+                self.pills.append((x,y)) #Add pill position to array
+
+        #Place Ghosts
+        for i in range(self.ghost_amount):
+            if(empty_cells):
+                x, y = random.choice(empty_cells)
+                self.grid[x, y] = 3 #Add ghost (3) to grid
+                self.ghosts.append((x, y)) #Add ghost position to array
+                empty_cells.remove((x, y)) #Remove empty cell from list
+
+        #Place food in left empty spaces
+        for x, y in empty_cells:
+            self.grid[x, y] = 4 #Add Food (4) to grid
+
+        #print(self.pills)
+        #print(self.ghosts)
--- Main.py
+++ Main.py
@@ -1,5 +1,13 @@
+import gymnasium as gym
+from CustomPacMan import CustomPacManEnv
+
 def main():
     print("hey there")
+    env = CustomPacManEnv(maze_size="normal")
+
+    print(env.grid)
+
+    env.close
 
 if __name__=="__main__":
     main()
